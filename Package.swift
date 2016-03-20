8c256b7a920abeec4e37f2439845975f0165a9c2
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-03-20 19:05:52 +0000

    added Socks target as well

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "Socks",
     targets: [
     	Target(name: "SocksCore"),
+		Target(name: "Socks", dependencies: [.Target(name: "SocksCore")]),
     	Target(name: "SocksCoreExampleServer", dependencies: [.Target(name: "SocksCore")]),
     	Target(name: "SocksCoreExampleClient", dependencies: [.Target(name: "SocksCore")]),
     ]
