713d7653151deca6c3fbf0fa7260ec58aaa12c96
Stephen Celis <stephen@stephencelis.com>
2016-04-01 16:16:47 -0400

    Remove `periodSeparatedString()`
    The performance benefits of raw enumeration are slight and the standard
    library implementation could surpass it at any time. We can favor less
    code (and fewer required tests) instead!
    
    Signed-off-by: Stephen Celis <stephen@stephencelis.com>

--- Sources/SocksCore/Conversions.swift
+++ Sources/SocksCore/Conversions.swift
@@ -12,21 +12,6 @@ func htons(value: CUnsignedShort) -> CUnsignedShort {
     return (value << 8) + (value >> 8)
 }
 
-extension Array {
-    
-    func periodSeparatedString() -> String {
-        let count = self.count
-        var str = ""
-        for (idx, el) in self.enumerated() {
-            str += "\(el)"
-            if idx < count - 1 {
-                str += "."
-            }
-        }
-        return str
-    }
-}
-
 extension String {
     
     public func toBytes() -> [UInt8] {
