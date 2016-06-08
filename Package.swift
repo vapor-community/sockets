d2048b0a95ce8ccd18be3eea3b2e8cfc283a18de
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-06-08 14:12:49 +0200

    Deps update

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     	Target(name: "SocksCore"),
 	Target(name: "Socks", dependencies: [.Target(name: "SocksCore")]),
         Target(name: "SocksCoreExampleTCPServer", dependencies: [.Target(name: "SocksCore")]),
-        Target(name: "SocksCoreExampleTCPKeepAliveServer", dependencies: [.Target(name: "SocksCore")]),
+        Target(name: "SocksCoreExampleTCPKeepAliveServer", dependencies: [.Target(name: "Socks")]),
     	Target(name: "SocksCoreExampleTCPClient", dependencies: [.Target(name: "SocksCore")]),
     	Target(name: "SocksExampleTCPServer", dependencies: [.Target(name: "Socks")]),
     	Target(name: "SocksExampleTCPClient", dependencies: [.Target(name: "Socks")]),
