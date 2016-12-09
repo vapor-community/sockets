6d5511b8c8ac23cdd152827bf48bb3dfa6b30ee3
andreasley <and@mac-mp-and.local>
2016-12-09 10:06:06 +0100

    UDPSocket calls `close()` on deinit

--- Sources/SocksCore/UDPSocket.swift
+++ Sources/SocksCore/UDPSocket.swift
@@ -10,10 +10,12 @@
     import Glibc
     private let socket_recvfrom = Glibc.recvfrom
     private let socket_sendto = Glibc.sendto
+    private let socket_close = Glibc.close
 #else
     import Darwin
     private let socket_recvfrom = Darwin.recvfrom
     private let socket_sendto = Darwin.sendto
+    private let socket_close = Darwin.close
 #endif
 
 public class UDPInternetSocket: InternetSocket {
@@ -21,6 +23,7 @@ public class UDPInternetSocket: InternetSocket {
     public let descriptor: Descriptor
     public let config: SocketConfig
     public let address: ResolvedInternetAddress
+    public private(set) var closed = false
 
     public required init(descriptor: Descriptor?, config: SocketConfig, address: ResolvedInternetAddress) throws {
 
@@ -39,6 +42,10 @@ public class UDPInternetSocket: InternetSocket {
         var conf: SocketConfig = .UDP(addressFamily: address.addressFamily)
         let resolved = try address.resolve(with: &conf)
         try self.init(descriptor: nil, config: conf, address: resolved)
+    }
+
+    deinit {
+        try? self.close()
     }
 
     public func recvfrom(maxBytes: Int = BufferCapacity) throws -> (data: [UInt8], sender: ResolvedInternetAddress) {
@@ -83,5 +90,13 @@ public class UDPInternetSocket: InternetSocket {
             destination.rawLen
         )
         guard sentLen == len else { throw SocksError(.sendFailedToSendAllBytes) }
+    }
+
+    public func close() throws {
+        if closed { return }
+        closed = true
+        if socket_close(self.descriptor) != 0 {
+            throw SocksError(.closeSocketFailed)
+        }
     }
 }
