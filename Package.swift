831efcf893cbb750498ec0b2c0f6966586edc0a6
Tanner Nelson <me@tanner.xyz>
2018-01-22 16:07:33 -0500

    switch to beta branch

--- Package.swift
+++ Package.swift
@@ -8,10 +8,10 @@ let package = Package(
     ],
     dependencies: [
         // Swift Promises, Futures, and Streams.
-        .package(url: "https://github.com/vapor/async.git", .branch("stream-refactor")),
+        .package(url: "https://github.com/vapor/async.git", .branch("beta")),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks.
-        .package(url: "https://github.com/vapor/core.git", .branch("stream-refactor")),
+        .package(url: "https://github.com/vapor/core.git", .branch("beta")),
     ],
     targets: [
         .target(name: "TCP", dependencies: ["Async", "Bits", "COperatingSystem", "Debugging"]),
