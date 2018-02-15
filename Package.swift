f4bd75b36858f2a560d482fcfab920074c17d7df
Tanner Nelson <me@tanner.xyz>
2018-02-14 20:05:38 -0500

    prerelease version updates

--- Package.swift
+++ Package.swift
@@ -8,10 +8,10 @@ let package = Package(
     ],
     dependencies: [
         // Swift Promises, Futures, and Streams.
-        .package(url: "https://github.com/vapor/async.git", .exact("1.0.0-beta.1")),
+        .package(url: "https://github.com/vapor/async.git", "1.0.0-beta.1"..<"1.0.0-beta.2"),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks.
-        .package(url: "https://github.com/vapor/core.git", .exact("3.0.0-beta.1")),
+        .package(url: "https://github.com/vapor/core.git", "3.0.0-beta.1"..<"3.0.0-beta.2"),
     ],
     targets: [
         .target(name: "TCP", dependencies: ["Async", "Bits", "COperatingSystem", "Debugging"]),
