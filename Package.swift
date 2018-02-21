a40c89565eb765a3975d8eb58322bded6979e9bc
Tanner Nelson <me@tanner.xyz>
2018-02-20 20:30:41 -0500

    bump

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Swift Promises, Futures, and Streams.
         .package(url: "https://github.com/vapor/async.git", "1.0.0-beta.1"..<"1.0.0-beta.2"),
 
-        // Core extensions, type-aliases, and functions that facilitate common tasks.
+        // Core extensions, type-aliases, and functions that facilitate common tasks. 
         .package(url: "https://github.com/vapor/core.git", "3.0.0-beta.2"..<"3.0.0-beta.3"),
     ],
     targets: [
